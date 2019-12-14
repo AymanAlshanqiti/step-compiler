adcdff06b8c39f4acd278c214dfce223e39273c5
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-12-14 21:57:09 +0300

    create Token class

---
+++ token.js
@@ -0,0 +1,10 @@
+
+class Token {
+  constructor(tid, value, category, position, line_number) {
+    this.tid = tid;
+    this.value = value;
+    this.category = category;
+    this.position = position;
+    this.line_number = line_number;
+  }
+}
